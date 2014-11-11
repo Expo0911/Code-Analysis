5529ab10cf8fc17e4afb063e9e9c315856ee9c16
Expo0911 <shibohou911@gmail.com>
2014-11-11 10:11:35 -0500

    final version

--- analysis.js
+++ analysis.js
@@ -44,10 +44,12 @@ var complexityBuilder =
 
 	report : function()
 	{
+		console.log("******************** End of Lists ********************");
+		console.log("**********Analysis results: **********");
 		console.log(
 		   ("Number of functions {0}\n" + 
 			"Cyclomatic complexity {1}\n" +
-			"Nesting Depth {2}\n" +
+			"Max Nesting Depth {2}\n" +
 			"Mean Parameters {3}\n" +
 			"Max Parameters {4}\n")
 			.format(complexityBuilder.Functions,
@@ -63,7 +65,7 @@ function complexity(filePath)
 {
 	var buf = fs.readFileSync(filePath, "utf8");
 	var result = esprima.parse(buf, options);
-	console.log("[*]. Lists of statistics results :");
+	console.log("**********Lists of statistics results : **********");
 	traverse(result, function (node) 
 	{	
 		//calculate the total number of Functions
@@ -80,13 +82,26 @@ function complexity(filePath)
 			}
 
 			complexityBuilder.MeanParameterCount += node.params.length; 
+
+			// calculate the max depth of scopes (nested ifs, loops, etc)
+			var result = {nestedDepth : 0};
+			visitDepth(node,0,result);
+			if(result.nestedDepth > complexityBuilder.MaxNestingDepth)
+			{
+				complexityBuilder.MaxNestingDepth = result.nestedDepth;
+			}
+			console.log( "MaxNestingDepth in this Fuction: {0}".format(complexityBuilder.MaxNestingDepth));
 		}
 		//calculate the complexity of Cyclomatic
-		if(node.type == 'IfStatement' || node.type === 'ForStatement' || node.type == 'ForInStatement')
+		if(node.type == 'IfStatement' || 
+			node.type === 'ForStatement' || 
+			node.type == 'ForInStatement' ||
+			node.type == 'DoStatement' ||
+			node.type == 'WhileStatement')
 		{
 			console.log( "Line : {0}, Type of Cyclomatic: {1}".format(node.loc.start.line, 
 				node.type));
-
+			//console.log(node.loc.end.line);
 			complexityBuilder.SimpleCyclomaticComplexity++;
 		}
 	});
@@ -150,6 +165,61 @@ if (!String.prototype.format) {
       ;
     });
   };
+}
+
+function visitDepth(node, depth, result)
+{
+    var key, child;
+    var children = 0;
+    for (key in node) {
+        if (node.hasOwnProperty(key)) 
+        {
+            child = node[key];
+            if (typeof child === 'object' && child !== null) 
+            {
+
+            	if(key === "alternate")
+            	{
+            		visitDepth(child, depth, result);   
+            	}
+            	else if(isDecision(child))
+            	{
+                	visitDepth(child, depth+1, result);           		
+            	}
+            	else
+            	{
+                	visitDepth(child, depth, result);   
+            	}
+       			children++;
+            }
+        }
+    }
+    if(children == 0 )
+    {
+    	if(result.nestedDepth < depth)
+    	{
+    		console.log("Current max depth: {0}".format(depth));
+    		result.nestedDepth = depth;	
+    	}
+    }
+}
+
+function isDecision (node)
+{
+	if(node.type == 'IfStatement' || 
+		node.type == 'ForInStatement' ||
+		node.type == 'ForStatement' ||
+		node.type == 'DoStatement' ||
+		node.type == 'WhileStatement'
+		)
+	{
+		console.log( "Embedded funtion(nested ifs, loops) exists in Line : {0}".format(node.loc.start.line));
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 main();
